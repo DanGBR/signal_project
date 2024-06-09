72456d141ed8bf74e8091fd671680e9fd440ffac
Daniel Gruber <danielgruber@daniels-mbp.speedport.ip>
2024-06-09 12:33:34 +0200

    Added UML Class Diagram

--- src/main/java/com/cardio_generator/outputs/ConsoleOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/ConsoleOutputStrategy.java
@@ -4,5 +4,6 @@ public class ConsoleOutputStrategy implements OutputStrategy {
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         System.out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
+
     }
 }
